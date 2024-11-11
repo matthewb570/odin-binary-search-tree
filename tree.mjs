c92b98b5afbecc3c8cb44c80481aa87b9b56342e
Matthew Burgess <boog397@gmail.com>
2024-11-10 21:26:27 -0600

    Add Tree class with methods for building the balanced binary search tree and finding elements in the tree

---
+++ tree.mjs
@@ -0,0 +1,53 @@
+import ArrayUtils from "./arrayUtils.mjs";
+import Node from "./node.mjs";
+
+class Tree {
+    root;
+
+    constructor(array) {
+        this.root = this.buildTree(array);
+    }
+
+    buildTree(array) {
+        // Set + spread operator creates a new array without duplicates; ArrayUtils.mergeSort() sorts the array
+        const arraySortedNoDuplicates = ArrayUtils.mergeSort([...new Set(array)]);
+
+        return this.#recursiveBuildTree(arraySortedNoDuplicates);
+    }
+
+    #recursiveBuildTree(array) {
+        if (array === undefined || array === null || array.length === 0) {
+            return null;
+        } else {
+            const arrayMidPoint = Math.floor(array.length / 2);
+
+            const rootNode = new Node(array[arrayMidPoint]);
+            rootNode.left = this.#recursiveBuildTree(array.slice(0, arrayMidPoint));
+            rootNode.right = this.#recursiveBuildTree(array.slice(arrayMidPoint + 1, array.length));
+
+            return rootNode;
+        }
+    }
+
+    find(value) {
+        return this.#recursiveFind(value, this.root);
+    }
+
+    #recursiveFind(value, root) {
+        if (root === null) {
+            return null;
+        }
+        
+        if (root.value === value) {
+            return root;
+        }
+
+        if (value > root.value) {
+            return this.#recursiveFind(value, root.right);
+        }
+
+        return this.#recursiveFind(value, root.left);
+    }
+}
+
+export default Tree;
