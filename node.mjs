d65fc4a5e730a8a77f7e1a08d7593ceb6aa1b0d8
Matthew Burgess <boog397@gmail.com>
2024-11-10 19:31:41 -0600

    Move Node class to correct file

---
+++ node.mjs
@@ -0,0 +1,13 @@
+class Node {
+    value;
+    left;
+    right;
+
+    constructor(value, left, right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+export default Node;
