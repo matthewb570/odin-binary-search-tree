c7250982732ba5a6cbfa7de53cfb85f301c90a29
Matthew Burgess <boog397@gmail.com>
2024-11-10 19:41:42 -0600

    Add ArrayUtils class with merge sort function

---
+++ arrayUtils.mjs
@@ -0,0 +1,38 @@
+class ArrayUtils {
+    static mergeSort(array) {
+        if (array.length === 1) {
+            return array;
+        }
+    
+        let arrayMidPoint = Math.round(array.length / 2);
+    
+        let leftArraySorted = mergeSort(array.slice(0, arrayMidPoint));
+        let rightArraySorted = mergeSort(array.slice(arrayMidPoint, array.length));
+    
+        let arraySorted = [];
+        let leftArrayIndex = 0;
+        let rightArrayIndex = 0;
+        while (leftArrayIndex < leftArraySorted.length || rightArrayIndex < rightArraySorted.length) {
+            let leftCandidate;
+            let rightCandidate;
+            if (leftArrayIndex < leftArraySorted.length) {
+                leftCandidate = leftArraySorted[leftArrayIndex];
+            }
+            if (rightArrayIndex < rightArraySorted.length) {
+                rightCandidate = rightArraySorted[rightArrayIndex];
+            }
+    
+            if (rightCandidate == undefined || (leftCandidate != undefined && leftCandidate <= rightCandidate)) {
+                arraySorted.push(leftCandidate);
+                leftArrayIndex++;
+            } else {
+                arraySorted.push(rightCandidate);
+                rightArrayIndex++;
+            }
+        }
+        
+        return arraySorted;
+    }
+}
+
+export default ArrayUtils;
