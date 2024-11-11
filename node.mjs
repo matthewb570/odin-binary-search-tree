7a4045ef42ecab9167fa39f7419dd056d88ea9fc
Matthew Burgess <boog397@gmail.com>
2024-11-10 21:25:20 -0600

    Add default values to Node constructor

--- node.mjs
+++ node.mjs
@@ -3,7 +3,7 @@ class Node {
     left;
     right;
 
-    constructor(value, left, right) {
+    constructor(value = null, left = null, right = null) {
         this.value = value;
         this.left = left;
         this.right = right;
